a652019cb3e2a7da0029f299a511fd59c88ef86f
wheatfox <enkerewpo@hotmail.com>
2024-08-03 00:52:21 +0800

    update

--- software/HELLO.C
+++ software/HELLO.C
@@ -1,45 +1,40 @@
-/*------------------------------------------------------------------------------
-HELLO.C
+#include "STC89C5xRC.H"
+#include <stdio.h>
 
-Copyright 1995-2005 Keil Software, Inc.
-------------------------------------------------------------------------------*/
+void Uart1_Init(void)	//9600bps@12.010MHz
+{
+	PCON |= 0x80;		//Enable SMOD bit
+	SCON = 0x50;		//8 bits and variable baudrate
+	AUXR |= 0x40;		//imer clock is 1T mode
+	AUXR &= 0xFE;		//UART 1 use Timer1 as baudrate generator
+	TMOD &= 0x0F;		//Set timer work mode
+	TMOD |= 0x20;		//Set timer work mode
+	TL1 = 0xB2;			//Initial timer value
+	TH1 = 0xB2;			//Set reload value
+	ET1 = 0;			//Disable Timer%d interrupt
+	TR1 = 1;			//Timer1 start run
+}
 
-#include <REG52.H>                /* special function register declarations   */
-                                  /* for the intended 8051 derivative         */
 
-#include <stdio.h>                /* prototype declarations for I/O functions */
+void _putchar(unsigned char m) { SBUF = m; }
 
+void delay() {
+  unsigned char i, j, k;
+  i = 8;
+  j = 1;
+  k = 243;
+  do {
+    do {
+      while (--k)
+        ;
+    } while (--j);
+  } while (--i);
+}
 
-#ifdef MONITOR51                         /* Debugging with Monitor-51 needs   */
-char code reserve [3] _at_ 0x23;         /* space for serial interrupt if     */
-#endif                                   /* Stop Exection with Serial Intr.   */
-                                         /* is enabled                        */
-
-
-/*------------------------------------------------
-The main C function.  Program execution starts
-here after stack initialization.
-------------------------------------------------*/
-void main (void) {
-
-/*------------------------------------------------
-Setup the serial port for 1200 baud at 16MHz.
-------------------------------------------------*/
-#ifndef MONITOR51
-    SCON  = 0x50;		        /* SCON: mode 1, 8-bit UART, enable rcvr      */
-    TMOD |= 0x20;               /* TMOD: timer 1, mode 2, 8-bit reload        */
-    TH1   = 221;                /* TH1:  reload value for 1200 baud @ 16MHz   */
-    TR1   = 1;                  /* TR1:  timer 1 run                          */
-    TI    = 1;                  /* TI:   set TI to send first char of UART    */
-#endif
-
-/*------------------------------------------------
-Note that an embedded program never exits (because
-there is no operating system to return to).  It
-must loop and execute forever.
-------------------------------------------------*/
+void main() {
+  Uart1_Init();
   while (1) {
-    P1 ^= 0x01;     		    /* Toggle P1.0 each time we print */
-    printf ("Hello World\n");   /* Print "Hello World" */
+    _putchar('p');
+    delay();
   }
 }
